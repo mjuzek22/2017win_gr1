5c9256f877e6e70800372fe3d0dd39625c56a4d7
mjuzek22 <32713609+mjuzek22@users.noreply.github.com>
2017-10-18 19:35:44 +0200

    Update kol1.py

--- kol1.py
+++ kol1.py
@@ -1,13 +1,24 @@
-#Banking simulator. Write a code in python that simulates the banking system. 
-#The program should:
-# - be able to create new banks
-# - store client information in banks
-# - allow for cash input and withdrawal
-# - allow for money transfer from client to client
-#If you can thing of any other features, you can add them.
-#This code shoud be runnable with 'python kol1.py'.
-#You don't need to use user input, just show me in the script that the structure of your code works.
-#If you have spare time you can implement: Command Line Interface, some kind of data storage, or even multiprocessing.
-#Do your best, show off with good, clean, well structured code - this is more important than number of features.
-#After you finish, be sure to UPLOAD this (add, commit, push) to the remote repository.
-#Good Luck
+
+class Client():
+	def __init__(self,name, surname, cash):
+		self.name = name
+		self.surname = surname
+		self.cash = cash
+
+class Bank():
+	def __init__(self):
+		self.clients = [1]
+
+	def new_client(self,name, surname, cash):
+		client = Client(name, surname, cash)
+		self.clients.append(client)
+
+	def cash_input(self,c_number, money):
+		self.clients[int(c_number)].cash += money
+
+	def cash_withdrawal(self, c_number,money):
+		self.clients[int(c_number)].cash +-= money
+	
+	def money_transfer(self, client_1, client_2, money):
+		self.clients[int(client_2)].cash += money
+		self.clients[int(client_1)].cash -= money
